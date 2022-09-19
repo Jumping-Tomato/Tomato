a4e4d57cc0370e26f7d4710368e520ac41e40db4
rongeegee <rongan.li2018@gmail.com>
2022-09-19 17:36:18 -0400

    rename variable

--- helpers/api/users-repo.js
+++ helpers/api/users-repo.js
@@ -1,22 +1,21 @@
 import dbPromise from "database/mongodb";
 
-let users;
+let db;
 dbPromise.then((value) => {
     const client = value;
-    const db = client.db("Tomato");
-    users = db.collection("users")
+    db = client.db("Tomato");
 });
 
 
 export const usersRepo = {
     getAll: async () => {
-        return await users.find({}).toArray();
+        return await db.collection("users").find({}).toArray();
     },
     getById: async id => {
-        return await users.findOne({"id": id});
+        return await db.collection("users").findOne({"id": id});
     },
     find: async email => {
-        return await users.findOne({"email": email});
+        return await db.collection("users").findOne({"email": email});
     } ,
     create,
     update,
@@ -25,24 +24,24 @@ export const usersRepo = {
 
 async function create(user) {
     // generate new user id
-    const newest_user = await users.find({}).sort({"id":-1}).limit(1).toArray();
+    const newest_user = await db.collection("users").find({}).sort({"id":-1}).limit(1).toArray();
     const max_id = newest_user[0]["id"];
     
     
-    user.id = users.count({}) ?  max_id + 1 : 1;
+    user.id = db.collection("users").count({}) ?  max_id + 1 : 1;
     // set date created and updated
     user.dateCreated = new Date().toISOString();
     user.dateUpdated = new Date().toISOString();
 
     // add and save user
-    users.insertOne(user).catch((error)=>{
+    db.collection("users").insertOne(user).catch((error)=>{
         throw `Unable to create user with the email "${user.email}"`;
     });
 }
 
 function update(id, params) {
     delete params['_id'];
-    return users.replaceOne({"id":id},params)
+    return db.collection("users").replaceOne({"id":id},params)
     .then((result)=>{
         console.log(`user with id "${id}" is updated is mongoDB`);
         console.log(result);
@@ -55,7 +54,7 @@ function update(id, params) {
 // prefixed with underscore '_' because 'delete' is a reserved word in javascript
 async function _delete(id) {
     try{
-        let result = await users.deleteOne({"id": id });
+        let result = await db.collection("users").deleteOne({"id": id });
         return result
     }
     catch(error){

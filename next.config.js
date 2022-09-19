c48d28520f673682a5d1a73f3a6ed5b9bdba6888
rongeegee <rongan.li2018@gmail.com>
2022-09-18 22:16:57 -0400

    change api route to solve cors policy error

--- next.config.js
+++ next.config.js
@@ -7,8 +7,8 @@ const nextConfig = {
   },
   publicRuntimeConfig: {
       apiUrl: process.env.NODE_ENV === 'development'
-          ? 'http://localhost:3000/api' // development api
-          : 'http://localhost:3000/api' // production api
+          ? '/api' // development api
+          : '/api' // production api
   }
 }
 

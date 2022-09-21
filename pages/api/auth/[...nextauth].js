902fb4a522d06d8c2a8f7efaea67c47bd0f5b259
rongeegee <rongan.li2018@gmail.com>
2022-09-20 22:03:18 -0400

    successfully using auth to login nad log out

---
+++ pages/api/auth/[...nextauth].js
@@ -0,0 +1,44 @@
+import NextAuth from 'next-auth'
+import CredentialProvider from 'next-auth/providers/credentials'
+
+export default NextAuth({
+  providers: [
+    CredentialProvider({
+        name: "credentials",
+        credentials: {
+                email: {label: "Email", type:"email", placeholder:"johnDoe@test.com"},
+                password: {label: "Password", type:"password"}
+        },
+        authorize: (credentials)=>{
+            //database look up
+            if(credentials.email === "ronganlihappyday@gmail.com" && credentials.password === "wakanda"){
+                return {
+                    id: 2,
+                    name: "Rongan",
+                    email: credentials.email
+                }
+            }
+            throw "Your mom";
+        }
+    })
+  ],
+  callbacks: {
+        jwt: ({token, user})=> {
+            if(user){
+                token.id = user.id;
+            }
+            return token;
+        },
+        session: ({session, token})=>{
+            if(token){
+                session.id = token.id;
+            }
+            return session;
+        }
+  },
+  secret: "test",
+  jwt: {
+    secret:"test",
+    encryption: true
+  }
+})

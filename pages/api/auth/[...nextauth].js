d17f944cd2933f7d4135c94a3ead5f9a2a8247ac
rongeegee <rongan.li2018@gmail.com>
2022-09-20 22:46:19 -0400

    indentation

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -2,43 +2,46 @@ import NextAuth from 'next-auth'
 import CredentialProvider from 'next-auth/providers/credentials'
 
 export default NextAuth({
-  providers: [
-    CredentialProvider({
-        name: "credentials",
-        credentials: {
-                email: {label: "Email", type:"email", placeholder:"johnDoe@test.com"},
-                password: {label: "Password", type:"password"}
-        },
-        authorize: (credentials)=>{
-            //database look up
-            if(credentials.email === "ronganlihappyday@gmail.com" && credentials.password === "wakanda"){
-                return {
-                    id: 2,
-                    name: "Rongan",
-                    email: credentials.email
+    providers: [
+        CredentialProvider({
+            name: "credentials",
+            credentials: {
+                    email: {label: "Email", type:"email", placeholder:"johnDoe@test.com"},
+                    password: {label: "Password", type:"password"}
+            },
+            authorize: (credentials)=>{
+                //database look up
+                if(credentials.email === "ronganlihappyday@gmail.com" && credentials.password === "wakanda"){
+                    return {
+                        id: 2,
+                        name: "Rongan",
+                        email: credentials.email
+                    }
                 }
+                throw "Your mom";
             }
-            throw "Your mom";
-        }
-    })
-  ],
-  callbacks: {
-        jwt: ({token, user})=> {
-            if(user){
-                token.id = user.id;
+        })
+    ],
+    callbacks: {
+            jwt: ({token, user})=> {
+                if(user){
+                    token.id = user.id;
+                }
+                return token;
+            },
+            session: ({session, token})=>{
+                if(token){
+                    session.id = token.id;
+                }
+                return session;
             }
-            return token;
-        },
-        session: ({session, token})=>{
-            if(token){
-                session.id = token.id;
-            }
-            return session;
-        }
-  },
-  secret: "test",
-  jwt: {
-    secret:"test",
-    encryption: true
-  }
+    },
+    pages: {
+        signIn: "/auth/signin"
+    },
+    secret: "test",
+    jwt: {
+        secret:"test",
+        encryption: true
+    }
 })

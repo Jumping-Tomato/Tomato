9142b8bc6f7cec5952c87b0ce50b914225d27093
rongeegee <rongan.li2018@gmail.com>
2022-09-29 15:52:47 -0400

    redirect to home page if you loginned users go to login or register page

--- middleware.js
+++ middleware.js
@@ -1,7 +1,18 @@
-export { default } from "next-auth/middleware"
+import { NextResponse } from 'next/server'
+import { getToken } from "next-auth/jwt"
 
-export const config = { 
-    matcher: [
-        '/((?!auth|api/register).*)(.+)'
-    ] 
+export async function middleware(req) {
+    const session = await getToken({ req, secret: "test" })
+    if (session && req.url.includes("/auth/signin") || req.url.includes("/auth/register")) {
+      return NextResponse.rewrite(new URL('/', req.url))
+    }
+    
+  
+}
+
+// Supports both a single string value or an array of matchers
+export const config = {
+  matcher: [
+    '/:path*'
+  ],
 }

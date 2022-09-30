1959fbd0038c518e3ee97c0e5cf61528aeac91a6
rongeegee <rongan.li2018@gmail.com>
2022-09-29 22:58:06 -0400

    allow rewrite to signin page when un-authenticated users access pages

--- middleware.js
+++ middleware.js
@@ -3,16 +3,23 @@ import { getToken } from "next-auth/jwt"
 
 export async function middleware(req) {
     const session = await getToken({ req, secret: "test" })
-    if (session && req.url.includes("/auth/signin") || req.url.includes("/auth/register")) {
+    const in_authentication_page = req.url.includes("/auth/signin") || req.url.includes("/auth/register");
+  
+    if (session && in_authentication_page) {
       return NextResponse.rewrite(new URL('/', req.url))
     }
-    
+    if (!session && !in_authentication_page){
+      return NextResponse.rewrite(new URL('/auth/signin', req.url))
+    } 
+
+    // If user is authenticated, continue.
+    return NextResponse.next()
   
 }
 
 // Supports both a single string value or an array of matchers
 export const config = {
   matcher: [
-    '/:path*'
+    '/((?!_next|api/auth|api/register).*)(.+)'
   ],
 }

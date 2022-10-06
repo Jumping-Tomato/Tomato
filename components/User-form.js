3fe54531963439c09661c2b30cdb2b87b3458d05
rongeegee <rongan.li2018@gmail.com>
2022-10-06 12:40:32 -0400

    Revert "reuse handleChange methdo on user-form component"
    This reverts commit 3e9cd8b3a88cc59e5bdf0aa37feb68f7a943eadf.

--- components/User-form.js
+++ components/User-form.js
@@ -2,15 +2,7 @@ import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
 import Alert from 'react-bootstrap/Alert';
 
-export default function UserForm({handleSubmit, formFields, error}) {
-    const handleChange = function (event){
-        let name = event.target.name;
-        let val = event.target.value;
-        setFormData({
-          ...formData,
-          [name]: val
-        });
-    }
+export default function UserForm({handleChange, handleSubmit, formFields, error}) {
     return (
         <Form onChange={handleChange}  onSubmit={handleSubmit} >  
             {

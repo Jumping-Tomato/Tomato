db638229d68865d1b273db81cabafecd3a654bb6
rongeegee <rongan.li2018@gmail.com>
2022-10-06 10:30:40 -0400

    clean code

--- components/User-form.js
+++ components/User-form.js
@@ -3,19 +3,16 @@ import Button from 'react-bootstrap/Button';
 import Alert from 'react-bootstrap/Alert';
 
 export default function UserForm({handleChange, handleSubmit, formFields, error}) {
-    var formArray = [];
-    for (let i = 0; i < formFields.length; i++){
-        let data = formFields[i];
-        formArray.push(
-            <Form.Group className="mb-3">
-                <Form.Label>{ data["label"]} </Form.Label>
-                <Form.Control type={ data["type"] } name={ data["name"] } placeholder={ data["placeholder"] }  required={ data["required"] } />
-            </Form.Group>
-        )
-    }
     return (
-        <Form onChange={handleChange}  onSubmit={handleSubmit} > 
-            { formArray }
+        <Form onChange={handleChange}  onSubmit={handleSubmit} >  
+            {
+                formFields.map(function(form) {
+                    return <Form.Group className="mb-3">
+                        <Form.Label>{ form["label"] }</Form.Label>
+                        <Form.Control type={ form["type"] } name={ form["name"] } placeholder={ form["placeholder"] }  required={ form["required"] } />
+                    </Form.Group>;
+                })
+            }
             { error && <Alert variant="danger"> {error} </Alert>}
             <Button variant="primary" type="submit">
                 Submit

ae364e78699d1cb0a190147872a241755298d6ce
rongeegee <rongan.li2018@gmail.com>
2022-10-09 20:31:17 -0400

    remove yarn.lock

--- database/mongodb-config.js
+++ database/mongodb-config.js
@@ -8,6 +8,7 @@ const options = {
 
 let client;
 let dbPromise;
+let db;
 
 if (!process.env.MONGODB_URI) {
   throw new Error('Please add your Mongo URI to .env.local')
@@ -27,7 +28,16 @@ if (process.env.NODE_ENV === 'development') {
   dbPromise = client.connect()
 }
 
+dbPromise.then((value) => {
+  const client = value;
+  db = client.db("Tomato");
+})
+.catch((error)=>{
+  console.error(error);
+});
+
 // Export a module-scoped MongoClient promise. By doing this in a
 // separate module, the client can be shared across functions.
 
-export default dbPromise
+export default db
+

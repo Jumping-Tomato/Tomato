6d159174f0785f5bda182957ae29ece80bdb06d1
rongeegee <rongan.li2018@gmail.com>
2022-11-06 00:22:51 -0400

    allow adding and removing choices for multiple choice question

--- components/QuestionCard.js
+++ components/QuestionCard.js
@@ -1,15 +1,14 @@
-import {Button, Card, Form} from 'react-bootstrap';
+import {Button, Card, Form, Row, Col} from 'react-bootstrap';
 import { useState } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faPlus, faMinus } from '@fortawesome/free-solid-svg-icons'
 
 export default function QuestionCard({props}) {
     const [questionType, setQuestionType] = useState("multiple choice");
     const [mcq, setMcq] = useState({
         question:"",
-        choice:{
-            a:"",
-            b:"",
-            c:"",
-            d:""
+        choices:{
+            a:""
         },
         correct_choice:""
     });
@@ -17,6 +16,22 @@ export default function QuestionCard({props}) {
     function toggleQuestionType (event){
         let value = event.target.value;
         setQuestionType(value);
+    }
+    function addChoice(event){
+        event.preventDefault();
+        const num_of_choices = Object.keys(mcq.choices).length;
+        if(num_of_choices < 5){
+            let new_mcq = Object.assign({}, mcq);
+            let new_choice = String.fromCharCode(94 + 3 + num_of_choices);
+            new_mcq.choices[new_choice] = "";
+            setMcq(new_mcq);
+        } 
+    }
+    function removeChoice(event){
+        const choice = event.currentTarget.value;
+        let new_mcq = Object.assign({}, mcq);
+        delete new_mcq.choices[choice];
+        setMcq(new_mcq);
     }
     return (
         <Card>
@@ -32,20 +47,51 @@ export default function QuestionCard({props}) {
                         </Form.Select>
                     </Form.Group>
                     {
-                        questionType == "multiple choice" 
+                        questionType == "multiple choice" || !questionType
                         ?
                         <>
                             {/* multiple choice */}
                             <Form.Group className="mb-3">
+                                <Form.Label>Question:</Form.Label>
                                 <Form.Control as="textarea" row={3} />
                             </Form.Group>
+                            {
+                                Object.entries(mcq.choices).map(([key, value], index)=>{
+                                    return(
+                                        <Form.Group className="mb-3" as={Row} >
+                                            <Col xs={1}>
+                                                <Form.Label>
+                                                    <h4>{key.toUpperCase()}:</h4>
+                                                </Form.Label>
+                                            </Col>
+                                            <Col xs={5}>
+                                                <Form.Control type="text" name="choice-a" value={value} required />
+                                            </Col>
+                                            <Col xs={5}>
+                                                {
+                                                    !index &&
+                                                    <button type="button" className="btn btn-sm btn-primary" onClick={addChoice}>
+                                                        <FontAwesomeIcon icon={faPlus} size="sm" />&nbsp; 
+                                                    </button>
+                                                }
+                                                {
+                                                    index > 0 && index == Object.keys(mcq.choices).length - 1 &&
+                                                    <button type="button" className="btn btn-sm btn-danger" value={key} onClick={removeChoice}>
+                                                        <FontAwesomeIcon icon={faMinus} size="sm" />&nbsp; 
+                                                    </button>
+                                                }                                               
+                                            </Col>
+                                        </Form.Group>
+                                        )
+                                })
+                            }                
                         </>
                         :
                         <>
                             <h1>short Answer placeholder</h1>
                         </>
                     }
-                    
+                    <br />
                     <Button variant="primary">Confirm</Button>&nbsp;
                     <Button variant="danger">Cancel</Button>  
                 </Form>   

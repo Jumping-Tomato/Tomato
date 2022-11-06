50171de56c8e5d11479d300e8ec1a9c95cad2785
rongeegee <rongan.li2018@gmail.com>
2022-11-06 16:58:00 -0500

    an index.js file for all custom components made by Rongan

---
+++ components/index.js
@@ -0,0 +1,15 @@
+import CreateTestForm from './CreateTestForm';
+import Footer from '/Footer';
+import QuestionCard from './QuestionCard';
+import Topbar from './Topbar';
+import UserForm from './User-form';
+
+
+
+export {
+    CreateTestForm,
+    Footer,
+    QuestionCard,
+    Topbar,
+    UserForm
+}

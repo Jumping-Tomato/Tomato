654dffa06da207a9ba1831d2de08e46687435ba0
rongeegee <rongan.li2018@gmail.com>
2022-11-27 23:33:28 -0500

    display all courses for students

--- pages/student/dashboard.js
+++ pages/student/dashboard.js
@@ -2,16 +2,12 @@ import { useSession } from 'next-auth/react'
 import Head from 'next/head'
 import global from 'styles/Global.module.scss'
 import { Topbar, Footer } from 'components'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faPlus } from '@fortawesome/free-solid-svg-icons'
-import {Spinner} from 'react-bootstrap';
 import { useState, useEffect } from 'react';
 import axios from 'axios';
 
-
 export default function StudentDashboard() {
     const { data: session } = useSession();
-    const [courses, setCourses] = useState("");
+    const [courses, setCourses] = useState([]);
     useEffect(() => {
         axios.get("/api/getCoursesForStudent")
         .then(function (response) {
@@ -22,7 +18,6 @@ export default function StudentDashboard() {
             
         }); 
     },[]);
-    const [showPopup, setShowPopup] = useState(false);
     return(
         <>
             <Topbar />
@@ -37,22 +32,23 @@ export default function StudentDashboard() {
                     <div className='row'>
                         <div className="col-12 pt-1">
                             <div className="col-12 pt-5">
-                                <button type="button" className="float-end btn btn-primary" onClick={() => setShowPopup(true)}>
-                                    <FontAwesomeIcon icon={faPlus} size="1x" />&nbsp;
-                                    Add
-                                </button>
-                            </div>
-                            <div className="col-12 pt-5">
-                                { !courses.length ?
-                                    <Spinner animation="border" />
-                                     :
-                                     <ul class="list-group">
+                                { 
+                                    courses.length > 0
+                                    &&
+                                    <ul class="list-group">
                                         {
-                                        courses.map((course) => {
-                                            return  (<li className="list-group-item" key={course._id}>
-                                                        {course.name}
-                                                    </li>);
-                                        })
+                                            courses.map((course) => {
+                                                return  (<li className="list-group-item" key={course._id}>
+                                                            <div class="row">
+                                                                <div className="col-6">
+                                                                    {course.name} 
+                                                                </div>
+                                                                <div className="col-6">
+                                                                    {course.semester}
+                                                                </div>
+                                                            </div>
+                                                        </li>);
+                                            })
                                         }
                                     </ul>
                                 }

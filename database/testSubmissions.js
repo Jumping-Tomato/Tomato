1c9f626bfe830077858a49f47cbd58c8105725d2
rongeegee <rongan.li2018@gmail.com>
2022-12-16 10:16:28 -0500

    backend to submit a question during a test

--- database/testSubmissions.js
+++ database/testSubmissions.js
@@ -1,5 +1,6 @@
 import dbPromise from "database/mongodb-config";
 const { ObjectId } = require('mongodb')
+var isEqual = require('lodash.isequal');
 
 let db;
 let client;
@@ -10,6 +11,7 @@ dbPromise.then((value) => {
 .catch((error)=>{
     console.error(error);
 });
+
 
 
 export const testSubmissions = {
@@ -84,30 +86,34 @@ async function submitTestQuestionById(test_submission_id, question_data){
                 {$push: {answered_questions: question_data}}
             );
             console.log(`${submit_answered_question.matchedCount} document(s) found in the testSubmissions collection with the _id ${test_submission_id}.`);
-            console.log(`${submit_answered_question.modifiedCount} document(s) was/were updated to include the reservation.`);
+            console.log(`${submit_answered_question.modifiedCount} document(s) was/were updated to append a new question to answered_questions array. question_id:  ${question_data.id}.`);
             const removed_unanswered_question = await testSubmissionsCollection.updateOne(
                 {"_id": ObjectId(test_submission_id)},
-                {$pop: {unanswered_questions: 1}}
+                {$pop: {unanswered_questions: -1}}
             );
             console.log(`${removed_unanswered_question.matchedCount} document(s) found in the testSubmissions collection with the _id ${test_submission_id}.`);
-            console.log(`${removed_unanswered_question.modifiedCount} document(s) was/were updated to include the reservation.`);
+            console.log(`${removed_unanswered_question.modifiedCount} document(s) was/were updated to remove the first question question from unanswered_questions array. question_id:  ${question_data.id}.`);
             const updatedTestSubmission = await testSubmissionsCollection.findOne(
-                {"_id": ObjectId(test_submission_id)}
+                {"_id": ObjectId(test_submission_id)},
+                { session }
             );
             const answered_questions = updatedTestSubmission.answered_questions;
             if(!answered_questions || !answered_questions.length){
+                console.error(`No new question added to answered_questions array.\nTest Submission Id: "${test_submission_id}"`)
                 await session.abortTransaction();
                 return;
             }
             const new_answer = answered_questions[answered_questions.length - 1]
-            if (!_.isEqual(new_answer,question_data)){
+            if (!isEqual(new_answer,question_data)){
+                console.error(`The answered question was NOT added to answered_questions array.\nTest Submission Id: "${test_submission_id}"`)
                 await session.abortTransaction();
                 return;
             }
             const unanswered_questions = updatedTestSubmission.unanswered_questions;
             if(unanswered_questions && unanswered_questions.length ){
                 const first_unanswered_question = unanswered_questions[0];
-                if(_.isEqual(first_unanswered_question,question_data)){
+                if(isEqual(first_unanswered_question,question_data)){
+                    console.error(`The answered question was NOT removed to unanswered_questions array.\nTest Submission Id: "${test_submission_id}"`)
                     await session.abortTransaction();
                     return;
                 }
@@ -120,7 +126,7 @@ async function submitTestQuestionById(test_submission_id, question_data){
     }
     catch(error){
         console.error(`Unable to submit the test question.\nError: "${error}"`);
-        console.error(`Submission: "${id}"`)
+        console.error(`Test Submission ID: "${id}"`)
         throw `Unable to submit the test question.\nError: "${error}"`;
     }
 }

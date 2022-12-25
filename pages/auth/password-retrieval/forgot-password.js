243d571ef1aa01d57869289cc0394fc63c993ea9
rongeegee <rongan.li2018@gmail.com>
2022-12-24 23:30:39 -0500

    Captcha on forgot-password page

--- pages/auth/password-retrieval/forgot-password.js
+++ pages/auth/password-retrieval/forgot-password.js
@@ -8,6 +8,7 @@ import axios from 'axios';
 import Head from 'next/head'
 import Modal from 'react-bootstrap/Modal';
 import global from 'styles/Global.module.scss'
+import ReCAPTCHA from "react-google-recaptcha";
 
 
 /*
@@ -23,6 +24,7 @@ export default function ForgotPasswordPage(){
     });
     const [showEmailPopup, setShowEmailPopup] = useState(false);
     const [error, setError] = useState("");
+    const [capctaValue, setCapctaValue] = useState("");
     const handleChange = function (event){
       let name = event.target.name;
       let val = event.target.value;
@@ -33,6 +35,10 @@ export default function ForgotPasswordPage(){
     }
     const handleSubmit = async function(event){
         event.preventDefault();
+        if(!capctaValue){
+          setError('Prove that you are not a bot.');
+          return;
+        }
         if(formData.newPassword1 !== formData.newPassword2){
             setError("Passwords do NOT match");
             return;
@@ -67,8 +73,18 @@ export default function ForgotPasswordPage(){
                <div className="col-lg-6 col-12 p-3">
                     <Form onChange={handleChange} onSubmit={handleSubmit} > 
                         <Form.Group className="mb-3">
-                            <Form.Label>Email</Form.Label>
-                            <Form.Control type="email" name="email" placeholder="email" required />
+                          <Form.Label>Email</Form.Label>
+                          <Form.Control type="email" name="email" placeholder="email" required />
+                        </Form.Group>
+                        <Form.Group className="mb-3">
+                          <ReCAPTCHA
+                            sitekey={process.env.NEXT_PUBLIC_GOOGLE_CAPTCHA_SECRET_KEY}
+                            onChange={
+                                (value)=>{
+                                    setCapctaValue(value);
+                                }
+                            }
+                          />
                         </Form.Group>
                         { error && <Alert variant="danger"> {error} </Alert>}
                         <Button variant="primary" type="submit">

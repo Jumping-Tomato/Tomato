ac3b0e3745662a50f64a4c8904859425ce031a98
rongeegee <rongan.li2018@gmail.com>
2022-12-28 17:22:12 -0500

    minor

--- pages/api/teacher/getAllTestScores.js
+++ pages/api/teacher/getAllTestScores.js
@@ -5,10 +5,10 @@ import { testSubmissions } from "database/testSubmissions";
 export default async function getAllTestScores(req, res) {
     const session = await getToken({ req})
     if(!session || session.role != "teacher"){
-        return res.status(403).json({ error: "forbidden" })
+        return res.status(403).json({ error: "forbidden" });
     }
-    const { test_id } = req.query;
     try {
+        const { test_id } = req.query;
         const score_data = await testSubmissions.getAllTestScores(test_id);
         return res.status(200).json({score_data: score_data});
     }

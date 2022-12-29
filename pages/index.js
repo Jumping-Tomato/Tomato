8cd4338f4faececa1fe371f2a57c57cded90a4f5
rongeegee <rongan.li2018@gmail.com>
2022-12-29 18:55:05 -0500

    temporary home page so far

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
-import Head from 'next/head'
-import global from 'styles/Global.module.scss'
-import {Topbar, Footer} from 'components'
+import Head from 'next/head';
+import global from 'styles/Global.module.scss';
+import {Topbar, Footer} from 'components';
+import Image from 'next/image';
 
 export default function Home() {
   return (
@@ -17,7 +18,14 @@ export default function Home() {
       <main className={global.main}>
         <div className='row'>
           <div className="col-lg-6 col-12 pt-5">
-          
+            <Image src="/images/home_1.png" alt="students and teachers in a classroom" width={566} height={696} />
+          </div>
+          <div className="col-lg-6 col-12 pt-5">
+            <h3 className="pt-5 lh-base">
+                Make teaching easier and more fun.
+                We simplify the job of every teacher
+                when it comes to testing.
+            </h3>
           </div>
         </div>
       </main>

84fcf8a0f4c8522a41492047905647f182d86e6c
rongeegee <rongan.li2018@gmail.com>
2023-01-27 13:29:46 -0500

    remove my name from footer

--- components/Footer.js
+++ components/Footer.js
@@ -4,13 +4,6 @@ export default function Footer() {
     return (
         <footer className={"row " + styles.footer}>
             <div className='col-lg-7 col-12 '>
-            Website designed and developed by &nbsp;
-            <a
-                href="https://rongeegee.github.io"
-                target="_blank"
-                rel="noopener noreferrer">
-                Rongan Li
-            </a>
             </div>
             <div className='col-lg-5 col-12'>
             Copyright © 2023 Jumping Tomato Inc. All Rights Reserved. 

410953ddfa8d2d528be6fd7da28ae09051d53a6b
rongeegee <rongan.li2018@gmail.com>
2023-02-05 14:42:03 -0500

    privacy policy page

---
+++ pages/privacy-policy.js
@@ -0,0 +1,107 @@
+import Head from 'next/head';
+import global from 'styles/Global.module.scss';
+import {Topbar, Footer} from 'components';
+import Image from 'next/image';
+
+export default function PrivacyPolicy() {
+  return (
+    <>
+    <Topbar />
+    <div className={global.container}>
+      <Head>
+        <title>Jumping Tomato | Cloud-Based software, Autograding Online Test System</title>
+        <meta name="Privacy Policy" content="At Jumping Tomato, we are dedicated to make the jobs of every teachers easier and more efficient by providing
+                                          the most adequating and most affordable autograding online test system" />
+        <link rel="icon" href="/images/logo.svg" />
+      </Head>
+
+      
+      <main className={global.main}>
+        <div className='row p-3'>
+            <h1>Privacy Policy</h1>
+            <p>Protecting your private information is our priority. This Statement of Privacy applies to Jumpingtomato.com and governs data collection and usage. The Jumpingtomato.com website is an autograding test taking site. By using the Jumpingtomato.com, you consent to the data practices described in this statement.</p>
+        </div>
+        <div className='row p-3'>
+            <h1>Collection of your Personal Information</h1>
+            <p>In order to better provide you with products and services offered on our Site, JumpingTomato.com may collect personally identifiable information, such as your:</p>
+            <ul className='ps-5'>
+              <li>First and Last Name</li>
+              <li>Email Address</li>
+            </ul>
+            <p>If you purchase Jumpingtomato.com’s products and services, we collect billing and credit card information. This information is used to complete the purchase transaction.</p>
+            <p>We do not collect any personal information about you unless you voluntarily provide it to us. However, you may be required to provide certain personal information to us when you elect to use certain products or services available on the Site. These may include: (a) registering for an account on our Site; (b) signing up for special offers from selected third parties; (c) sending us an email message; (d) submitting your credit card or other payment information when ordering and purchasing products and services on our Site. To wit, we will use your information for, but not limited to, communicating with you in relation to services and/or products you have requested from us. We also may gather additional personal or non-personal information in the future.</p>
+        </div>
+        <div className='row p-3'>
+            <h1>Use of your Personal Information</h1>
+            <p>Jumpingtomato.com collects and uses your personal information to operate its website(s) and deliver the services you have requested.</p>
+            <p>Jumpingtomato.com may also use your personally identifiable information to inform you of other products or services available from Jumpingtomato.com and its affiliates.</p>
+        </div>
+        <div className='row p-3'>
+            <h1>Sharing Information with Third Parties</h1>
+            <p>Jumpingtomato.com does not sell, rent or lease its customer lists to third parties.</p>
+            <p>Jumpingtomato.com may share data with trusted partners to help perform statistical analysis, send you email or postal mail, provide customer support, or arrange for deliveries. All such third parties are prohibited from using your personal information except to provide these services to Jumpingtomato.com, and they are required to maintain the confidentiality of your information.</p>
+            <p>Jumpingtomato.com may disclose your personal information, without notice, if required to do so by law or in the good faith belief that such action is necessary to: (a) conform to the edicts of the law or comply with legal process served on Jumpingtomato.com; (b) protect and defend the rights or property of Jumpingtomato.com; and/or (c) act under exigent circumstances to protect the personal safety of users of Jumpingtomato.com, or the public.</p>
+        </div>
+        <div className='row p-3'>
+            <h1>Tracking User Behavior</h1>
+            <p>Jumpingtomato.com may keep track of the websites and pages our users visit within Jumpingtomato.com, in order to determine which of our services are the most popular. This data is used to deliver customized content and advertising within our site to customers whose behavior indicates that they are interested in a particular subject area.</p>
+        </div>
+        <div className='row p-3'>
+            <h1>Automatically Collected Information</h1>
+            <p>Information about your computer hardware and software may be automatically collected by Jumpingtomato.com. This information can include: your IP address, browser type, domain names, access times and referring website addresses. This information is used for the operation of the service, to maintain quality of the service, and to provide general statistics regarding use of the Jumpingtomato.com.</p>
+        </div>
+        <div className='row p-3'>
+            <h1>Use of Cookies</h1>
+            <p>Jumpingtomato.com may use &quot;cookies&quot; to help you personalize your online experience. A cookie is a text file that is placed on your hard disk by a web page server. Cookies cannot be used to run programs or deliver viruses to your computer. Cookies are uniquely assigned to you, and can only be read by a web server in the domain that issued the cookie to you.</p>
+            <p>One of the primary purposes of cookies is to provide a convenience feature to save you time. The purpose of a cookie is to tell the Web server that you have returned to a specific page. For example, if you register with Jumpingtomato.com site or services, a cookie helps Jumpingtomato.com to recall your specific information on subsequent visits. This simplifies the process of recording your personal information, such as email addresses, user roles, and so on. When you return to the same Jumpingtomato.com website, the information you previously provided can be retrieved, so you can easily use the Jumpingtomato.com features that you customized.</p>
+            <p>You have the ability to accept or decline cookies. Most Web browsers automatically accept cookies, but you can usually modify your browser setting to decline cookies if you prefer. If you choose to decline cookies, you may not be able to fully experience the interactive features of the Jumpingtomato.com services or websites you visit.</p>
+        </div>
+        <div className='row p-3'>
+            <h1>Links</h1>
+            <p>This website may contain links to other sites. Please be aware that we are not responsible for the content or privacy practices of such other sites. We encourage our users to be aware when they leave our site and to read the privacy statements of any other site that collects personally identifiable information.</p>
+        </div>
+        <div className='row p-3'>
+            <h1>Security of your Personal Information</h1>
+            <p>Jumpingtomato.com secures your personal information from unauthorized access, use, or disclosure. Jumpingtomato.com uses the following methods for this purpose:</p>
+            <ul className='ps-5'>
+              <li>SSL Protocol</li>
+            </ul>
+            <p>When personal information (such as a credit card number) is transmitted to other websites, it is protected through the use of encryption, such as the Secure Sockets Layer (SSL) protocol.</p>
+            <p>We strive to take appropriate security measures to protect against unauthorized access to or alteration of your personal information. Unfortunately, no data transmission over the Internet or any wireless network can be guaranteed to be 100% secure. As a result, while we strive to protect your personal information, you acknowledge that: (a) there are security and privacy limitations inherent to the Internet which are beyond our control; and (b) security, integrity, and privacy of any and all information and data exchanged between you and us through this Site cannot be guaranteed.</p>
+        </div>
+        <div className='row p-3'>
+            <h1>Children Under Thirteen</h1>
+            <p>Jumpingtomato.com does not knowingly collect personally identifiable information from children under the age of thirteen. If you are under the age of thirteen, you must ask your parent or guardian for permission to use this website.</p>
+        </div>
+        <div className='row p-3'>
+            <h1>E-mail Communications</h1>
+            <p>From time to time, Jumpingtomato.com may contact you via email for the purpose of providing announcements, promotional offers, alerts, confirmations, surveys, and/or other general communication. In order to improve our Services, we may receive a notification when you open an email from Jumpingtomato.com or click on a link therein.</p>
+            <p>If you would like to stop receiving marketing or promotional communications via email from Jumpingtomato.com, you may opt out of such communications by clicking on the UNSUBSCRIBE button.</p>
+        </div>
+        <div className='row p-3'>
+            <h1>External Data Storage Sites</h1>
+            <p>We may store your data on servers provided by third party hosting vendors with whom we have contracted.</p>
+        </div>
+        <div className='row p-3'>
+            <h1>Changes to this Statement</h1>
+            <p>Jumpingtomato.com reserves the right to change this Privacy Policy from time to time. We will notify you about significant changes in the way we treat personal information by sending a notice to the primary email address specified in your account, by placing a prominent notice on our site, and/or by updating any privacy information on this page. Your continued use of the Site and/or Services available through this Site after such modifications will constitute your: (a) acknowledgment of the modified Privacy Policy; and (b) agreement to abide and be bound by that Policy.</p>
+        </div>
+        <div className='row p-3'>
+            <h1>Contact Information</h1>
+            <p>Jumpingtomato.com welcomes your questions or comments regarding this Statement of Privacy. If you believe that Jumpingtomato.com has not adhered to this Statement, please contact Jumpingtomato.com at:</p>
+            <h6>Email:</h6>
+            <p className='ps-5'>support@jumpingtomato.com</p>
+            <p>Effective as of February 5, 2023</p>
+        </div>
+      </main>
+    </div>
+    <Footer />
+    </>
+  )
+}
+
+export async function getStaticProps() {
+  return {
+    props: {},
+  }
+}

711ec18a9de840ba135eddba635b4c09563ce672
rongeegee <rongan.li2018@gmail.com>
2023-04-28 00:30:08 -0400

    footer change

--- components/Footer.js
+++ components/Footer.js
@@ -1,25 +1,36 @@
 import styles from '../styles/Footer.module.scss'
 import  Link  from 'next/link';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faHeart } from '@fortawesome/free-solid-svg-icons';
 
 export default function Footer() {
     return (
-        <footer className={"row " + styles.footer}>
-            <div className='col-lg-4 col-12'>
-                <span style={{fontSize:"12px"}}>
-                    Copyright © 2023 Jumpingtomato.com. All Rights Reserved. 
-                </span>
+        <footer className={styles.footer}>
+            <div className="row">
+                <div className='col-lg-4 col-12'>
+                    <span style={{fontSize:"12px"}}>
+                        Copyright © 2023 Jumpingtomato.com. All Rights Reserved. 
+                    </span>
+                </div>
+                <div className={'col-lg-8 col-12 row ' + styles.row}>
+                    <Link href="/contact" legacyBehavior={false} className={'col-4 col-md-2' + ' ' +styles.link}>
+                        Contact Us
+                    </Link>
+                    <Link href="/pricing" legacyBehavior={false} className={'col-4 col-md-2' + ' ' + styles.link}>
+                        Pricing
+                    </Link>
+                    <Link href="/privacy-policy" legacyBehavior={false} className={'col-4 col-md-2' + ' ' + styles.link}>
+                        Privacy Policy
+                    </Link>
+                </div>
             </div>
-            <div className={'col-lg-8 col-12 row ' + styles.row}>
-                <Link href="/contact" legacyBehavior={false} className={'col-4 col-md-2' + ' ' +styles.link}>
-                    Contact Us
-                </Link>
-                <Link href="/pricing" legacyBehavior={false} className={'col-4 col-md-2' + ' ' + styles.link}>
-                    Pricing
-                </Link>
-                <Link href="/privacy-policy" legacyBehavior={false} className={'col-4 col-md-2' + ' ' + styles.link}>
-                    Privacy Policy
-                </Link>
-            </div>  
+            <div className="row">
+                <div className='col-12'>
+                    <span style={{fontSize:"12px"}}>
+                        Made with <FontAwesomeIcon icon={faHeart} size="1x" /> in NYC
+                    </span>
+                </div>
+            </div>
         </footer>
     )
 }

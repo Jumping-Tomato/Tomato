e5a947460da87942ef77d2372c13b3b05e2454e8
rongeegee <rongan.li2018@gmail.com>
2023-08-05 14:33:56 -0400

    Revert "prevent dbPromise imported as null"
    This reverts commit d9ae2acef7f5b20e1f0187fc5b303fae220e0b78.

--- database/mongodb-config.js
+++ database/mongodb-config.js
@@ -6,36 +6,26 @@ const options = {
   useNewUrlParser: true,
 }
 
-
-let dbPromise = getDbPromise().then((promise)=>{
-  return promise;
-})
-.catch((error=>{
-  console.error(error);
-}));
+let client;
+let dbPromise;
 
 if (!process.env.MONGODB_URI) {
   throw new Error('Please add your Mongo URI to .env.local')
 }
 
-async function getDbPromise(){
-  let client;
-  if (process.env.NODE_ENV === 'development') {
-    // In development mode, use a global variable so that the value
-    // is preserved across module reloads caused by HMR (Hot Module Replacement).
-    if (!global._mongoClientPromise) {
-      client = new MongoClient(uri, options)
-      global._mongoClientPromise = await client.connect()
-    }
-    return global._mongoClientPromise
-  } else {
-    // In production mode, it's best to not use a global variable.
+if (process.env.NODE_ENV === 'development') {
+  // In development mode, use a global variable so that the value
+  // is preserved across module reloads caused by HMR (Hot Module Replacement).
+  if (!global._mongoClientPromise) {
     client = new MongoClient(uri, options)
-    let promise = await client.connect();
-    return promise;
+    global._mongoClientPromise = client.connect()
   }
+  dbPromise = global._mongoClientPromise
+} else {
+  // In production mode, it's best to not use a global variable.
+  client = new MongoClient(uri, options)
+  dbPromise = client.connect()
 }
-
 
 // Export a module-scoped MongoClient promise. By doing this in a
 // separate module, the client can be shared across functions.
